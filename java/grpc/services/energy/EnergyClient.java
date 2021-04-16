e7eea5ade43879f32e4cc66aec4d82a225dd7d7d
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-16 09:30:05 +0100

    Create EnergyClient.java

---
+++ java/grpc/services/energy/EnergyClient.java
@@ -0,0 +1,32 @@
+package grpc.services.energy;
+
+import grpc.services.energy.EnergyServiceGrpc.EnergyServiceBlockingStub;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class EnergyClient {
+	
+	private static EnergyServiceGrpc.EnergyServiceBlockingStub blockingStub;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051).usePlaintext().build();
+
+		blockingStub = EnergyServiceGrpc.newBlockingStub(channel);
+
+		//Create Request messages for use within the main method
+		
+
+		//Call the method RPC from within main
+		
+
+		//Call the method RPC from within main
+		
+
+		//Call the login RPC from  method
+	
+
+		//Call the logout RPC from  method
+		
+	}
+}
