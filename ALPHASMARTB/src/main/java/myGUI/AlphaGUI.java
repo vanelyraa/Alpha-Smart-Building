fe1285605ef3c7a4fed9e386bd034ef034aff91a
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-19 08:49:58 +0100

    Create AlphaGUI.java

---
+++ ALPHASMARTB/src/main/java/myGUI/AlphaGUI.java
@@ -0,0 +1,43 @@
+package myGUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class AlphaGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AlphaGUI window = new AlphaGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AlphaGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1047, 622);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
