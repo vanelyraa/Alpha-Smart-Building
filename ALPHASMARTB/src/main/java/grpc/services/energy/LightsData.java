d2fee3a5428f77a6dac18306ac5d30197ce5eceb
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-19 08:49:24 +0100

    Update LightsData.java

--- ALPHASMARTB/src/main/java/grpc/services/energy/LightsData.java
+++ ALPHASMARTB/src/main/java/grpc/services/energy/LightsData.java
@@ -1,5 +1,25 @@
 package grpc.services.energy;
 
-public class LightsData {
+import java.util.ArrayList;
 
+
+public class LightsData extends ArrayList<Lights> {
+
+	public static LightsData lightsdata;
+
+    public static LightsData getInstance() {
+        if (lightsdata == null) {
+        	lightsdata = new LightsData();
+        }
+        return lightsdata;
+    }
+
+    public LightsData() {
+        this.add(grpc.services.energy.Lights.newBuilder()
+                .setLightId(1)
+                .setLightStatus("ON")
+                .setAdjust(1)
+                .build());
+
+}
 }
