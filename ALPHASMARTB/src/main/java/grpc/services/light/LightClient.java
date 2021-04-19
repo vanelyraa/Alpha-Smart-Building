ac2f6777f78ab5f9a5b49daea893ea0a5fa74c23
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-20 00:39:21 +0100

    Update LightClient.java

--- ALPHASMARTB/src/main/java/grpc/services/light/LightClient.java
+++ ALPHASMARTB/src/main/java/grpc/services/light/LightClient.java
@@ -6,13 +6,29 @@ import io.grpc.ManagedChannelBuilder;
 public class LightClient {
 
 	private static LightServiceGrpc.LightServiceBlockingStub blockingStub;
+	private static LightServiceGrpc.LightServiceStub asyncStub;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50053).usePlaintext().build();
 
 		blockingStub = LightServiceGrpc.newBlockingStub(channel);
+		asyncStub = LightServiceGrpc.newStub(channel);
+		
+		lighting();
+		LightsOnOff();
+		lightIntensity();
+	}
+	
+	public static void lighting(){
+		
+	}
 
+	public static void LightsOnOff(){
+		
+	}
+	
+	public static void lightIntensity(){
 		
 	}
 }
