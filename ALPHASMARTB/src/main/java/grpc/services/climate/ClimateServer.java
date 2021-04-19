ea46a1ba46def031ef834c773179b29a97b53611
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-19 23:09:23 +0100

    Update ClimateServer.java

--- ALPHASMARTB/src/main/java/grpc/services/climate/ClimateServer.java
+++ ALPHASMARTB/src/main/java/grpc/services/climate/ClimateServer.java
@@ -1,17 +1,17 @@
 package grpc.services.climate;
 
 import java.io.IOException;
-import java.util.ArrayList;
+
 import grpc.services.climate.ClimateServiceGrpc.ClimateServiceImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
-public class ClimateServer extends ClimateServiceImplBase {
+public class ClimateServer extends ClimateServiceImplBase  {
 	
-public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, InterruptedException {
 		
-		System.out.println("Starting gRPC Climate Server");
+		System.out.println("Starting gRPC Server");
 		ClimateServer climateserver = new ClimateServer();
 
 		int port = 50052;
@@ -32,61 +32,61 @@ public static void main(String[] args) {
 		catch(InterruptedException e) {
 			e.printStackTrace();
 		}
-}//main
 
-@Override
-public void showHvacStatus(HvacStatusRequest request, StreamObserver<HvacResponse> responseObserver) {
-	//loop all HVACs
-	for (grpc.services.climate.Hvac hvac : Hvacs.getInstance()) {
-        if (hvac.getHvacId() == request.getHvacId()) {
-        	HvacResponse response = HvacResponse.newBuilder().setHvac(hvac).build();
-        	responseObserver.onNext(response);
-        	responseObserver.onCompleted();
-            return;
-        }
-    }
-	//super.showHvacStatus(request, responseObserver);
-}
+	}//main
 
-@Override
-public void setHvacTemp(TempRequest request, StreamObserver<HvacResponse> responseObserver) {
-	ArrayList<grpc.services.climate.Hvac> tempList = Hvacs.getInstance();
-	int hvactemp = request.getTemperature();
-    
-	for (int i = 0; i < tempList.size(); i++) {
-		grpc.services.climate.Hvac hvac = (grpc.services.climate.Hvac) tempList.get(i);
-        Hvacs.hvacs.clear();
-        	
-        Hvacs.hvacs.add(grpc.services.climate.Hvac.newBuilder()
-        			.setHvacId(hvac.getHvacId())
-        			.setStatus(("On"))
-        			.setTemperature(hvactemp)
-        			.build());
-        }
-    
-    for (grpc.services.climate.Hvac hvac : Hvacs.hvacs) {
-    	HvacResponse response = HvacResponse.newBuilder().setHvac(hvac).build();
-    	responseObserver.onNext(response);
-    	responseObserver.onCompleted();
-    	return;
-    }
-	//super.setHvacTemp(request, responseObserver);
-}
 
-@Override
-public void checkCO(CoLevelRequest request, StreamObserver<ExtractionResponse> responseObserver) {
-	for (grpc.services.climate.CoLevel coLevel : CoLevels.getInstance()) {
-        if (coLevel.getBuilding() == request.getBuilding()) {
-        	ExtractionResponse response = ExtractionResponse.newBuilder().setCoLevel(coLevel).build();
-        	responseObserver.onNext(response);
-        	responseObserver.onCompleted();
-            return;
+	@Override
+	public void hvacOnOff(SwitchRequest request, StreamObserver<SwitchResponse> responseObserver) {
+		
+		System.out.println("Receiving request to turn on HVAC!");
+
+        boolean OnOffH = request.getPower();
+        if (OnOffH) {
+        	System.out.println("Setting power to on!");
         }
+        else {
+        	System.out.println("Setting power to off!");
+        }
+        
+        SwitchResponse response = SwitchResponse.newBuilder().setPower(OnOffH).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
 	}
+
+	@Override
+	public void hvacTemperature(HvacRequest request, StreamObserver<HvacResponse> responseObserver) {
+
+		int newTemp = request.getTemp();
+		 System.out.println("Requested temperature change to -> " + newTemp + " C");
+		 System.out.println("Temperature adjusted");
+		 responseObserver.onNext(HvacResponse.newBuilder().setTemp(newTemp).build());
+		 
+		 responseObserver.onCompleted();
+
+	}
+
+	@Override
+	public void checkCO(CoLevelRequest request, StreamObserver<ExtractionResponse> responseObserver) {
+
+System.out.println("Checking CO levels in the office!");
+		
+		int CoLv = request.getLevel();
+       
+        if (CoLv > 40) {
+        	System.out.println("Turn extraction on!");
+        }
+        else {
+        	System.out.println("Turn extraction off!");
+        }
+        
+        ExtractionResponse response = ExtractionResponse.newBuilder().setLevel(CoLv).build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+	}
+
 }
-	//super.checkCO(request, responseObserver);
-}
-
-
-
-
