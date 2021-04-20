243a7dc028d838eb88696717c9515a69aa6ce827
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-20 12:28:54 +0100

    Update UtilityServiceGrpc.java

--- ALPHASMARTB/src/main/java/grpc/services/utility/UtilityServiceGrpc.java
+++ ALPHASMARTB/src/main/java/grpc/services/utility/UtilityServiceGrpc.java
@@ -19,7 +19,7 @@ import static io.grpc.stub.ServerCalls.asyncUnimplementedUnaryCall;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.15.0)",
-    comments = "Source: Utility.proto")
+    comments = "Source: UtilityService.proto")
 public final class UtilityServiceGrpc {
 
   private UtilityServiceGrpc() {}
