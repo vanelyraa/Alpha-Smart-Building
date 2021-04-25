6a6ba19bc3ee27e7290daa6b4be8d5f4895ed488
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-25 18:18:51 +0100

    Update ClimateServer.java

--- ALPHASMARTB/src/main/java/grpc/services/climate/ClimateServer.java
+++ ALPHASMARTB/src/main/java/grpc/services/climate/ClimateServer.java
@@ -2,6 +2,7 @@ package grpc.services.climate;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 import java.util.logging.Logger;
 
@@ -17,88 +18,34 @@ import io.grpc.stub.StreamObserver;
 
 public class ClimateServer extends ClimateServiceImplBase  {
 	
-	public static int climatePort;
-	private static final Logger logger = Logger.getLogger(ClimateServer.class.getName());
+	public static void main(String[] args) throws IOException, InterruptedException {
 	
-	private static class Listener implements ServiceListener {
-		 
-        public void serviceAdded(ServiceEvent event) {
-            System.out.println("Service added: " + event.getInfo());
-        }
-
+	System.out.println("Starting gRPC Climate Server");
+	
+	// Create & Register utilities service with jmDNS
+	try {
+		int PORT = 50051;
+		JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+        ServiceInfo serviceInfo = ServiceInfo.create("_climate._tcp.local.", "climate", PORT, "Climate server");
+        jmdns.registerService(serviceInfo);
+        ClimateServer climateServer = new ClimateServer();
+        Server server = ServerBuilder.forPort(PORT)
+                .addService(climateServer)
+                .build()
+                .start();
+        System.out.println("Climate server started, listening on " + PORT);
+        server.awaitTermination();
         
-        public void serviceRemoved(ServiceEvent event) {
-            System.out.println("Service removed: " + event.getInfo());
-        }
-
-        
-        public void serviceResolved(ServiceEvent event) {
-        	System.out.println("Service resolved: " + event.getInfo());
-            System.out.println("Get Name: " + event.getName()+" PORT: "+event.getInfo().getPort());
-            
-            //Start GRPC server with discovered device
-            if(event.getName().equals("Climate")) {
-            	System.out.println("Found climate port: " + event.getInfo().getPort());
-	       		try {
-	       			climatePort = event.getInfo().getPort();
-					startGRPC(event.getInfo().getPort());
-	       		} 
-	       		catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} 
-	       		catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-
-            }
-          
-
-        }
+	} catch (UnknownHostException e) {
+		System.out.println(e.getMessage());
+        e.printStackTrace();
+	} catch (IOException e) {
+        System.out.println(e.getMessage());
+        e.printStackTrace();
     }
 	
+}
 	
-	public static void main(String[] args) throws IOException, InterruptedException {
-		startDiscovery();
-	}
-		
-	public static void startDiscovery() throws IOException, InterruptedException {
-		System.out.println("Starting climate gRPC Server");
-		
-		try {
-			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
-			jmdns.addServiceListener("_http._tcp.local.", new Listener());
-		    System.out.println("Listener working");
-		    
-		    Thread.sleep(20000);
-			
-		} catch (UnknownHostException e) {
-			System.out.println(e.getMessage());
-            e.printStackTrace();
-		} catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-
-	}//main
-
-	public int getClimatePort() {
-		return climatePort;
-	}
-
-	public void settClimatePort(int climatePort) {
-		ClimateServer.climatePort = climatePort;
-	}
-	
-	public static void startGRPC(int portNumber) throws IOException, InterruptedException {
-		ClimateServer climateServer = new ClimateServer();
-		    
-		Server server = ServerBuilder.forPort(portNumber).addService(climateServer).build().start();
-		logger.info("ClimateServer started, listening on " + portNumber);		     
-		server.awaitTermination();
-	 }
-
 	@Override
 	public void hvacOnOff(SwitchRequest request, StreamObserver<SwitchResponse> responseObserver) {
 		
@@ -139,15 +86,18 @@ public class ClimateServer extends ClimateServiceImplBase  {
 		int CoLv = request.getLevel();
        
         if (CoLv > 40) {
+        	responseObserver.onNext(ExtractionResponse.newBuilder().setLevel(CoLv).build());
         	System.out.println("Turn extraction on!");
+        	
         }
         else {
+        	responseObserver.onNext(ExtractionResponse.newBuilder().setLevel(CoLv).build());
         	System.out.println("Turn extraction off!");
         }
         
-        ExtractionResponse response = ExtractionResponse.newBuilder().setLevel(CoLv).build();
-
-        responseObserver.onNext(response);
+        //ExtractionResponse response = ExtractionResponse.newBuilder().setLevel(CoLv).build();
+        //System.out.println("this is response   "+response);
+        //responseObserver.onNext(response);
         responseObserver.onCompleted();
 
 	}
