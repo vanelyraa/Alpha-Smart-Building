76c23ad74f45638bb38b7df116dae1cba4e8e79b
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-25 18:19:08 +0100

    Update LightData.java

--- ALPHASMARTB/src/main/java/grpc/services/light/LightData.java
+++ ALPHASMARTB/src/main/java/grpc/services/light/LightData.java
@@ -1,16 +1,20 @@
 package grpc.services.light;
 
-public class LightData {
+import java.util.ArrayList;
+
+public class LightData extends ArrayList<LightingResponse> {
 
 	private String type, typeId;
 	private boolean on;
 	private int intensity;
+	public static LightData lights;
+
 	
 	public LightData() {
 		this.type="Lighs";
 		this.on=true;
 		this.typeId="Light ID";
-		this.intensity=40;
+		this.intensity=4;
 	}
 
 	public String gettype() {
@@ -45,4 +49,22 @@ public class LightData {
 		this.intensity = intensity;
 	}
 	
+	public static LightData getInstance() {
+		if(lights == null) {
+			lights = new LightData();
+		}
+		return lights;
+	}
+	
+	public void LightDataArray() {
+		
+		//ArrayList<LightingResponse> listOfLights = new ArrayList<>();
+		this.add(LightingResponse.newBuilder().setLightId("Conference Room").setIntensity(1).setStatus("On").build());
+		this.add(LightingResponse.newBuilder().setLightId("Cafeteria").setIntensity(1).setStatus("On").build());
+		this.add(LightingResponse.newBuilder().setLightId("Room 1").setIntensity(3).setStatus("On").build());
+		this.add(LightingResponse.newBuilder().setLightId("Room 2").setIntensity(4).setStatus("On").build());
+		this.add(LightingResponse.newBuilder().setLightId("Toilets").setIntensity(2).setStatus("On").build());
+	}
 }
+
+	
