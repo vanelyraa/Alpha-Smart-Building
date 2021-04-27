f2a4a7d2fd6c0a2ea23bfae4b2643d2879667545
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-27 11:48:28 +0100

    Update ServiceRegistration.java

--- ALPHASMARTB/src/main/java/jmDNS/ServiceRegistration.java
+++ ALPHASMARTB/src/main/java/jmDNS/ServiceRegistration.java
@@ -2,48 +2,51 @@ package jmDNS;
 
 import java.io.IOException;
 import java.net.InetAddress;
-
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceInfo;
 
-import grpc.services.light.LightData;
 
 public class ServiceRegistration {
+	
 	public static JmDNS jmdns;
-
+	
 	 public static void main(String[] args) throws InterruptedException {
 	 }
-
-	 public void jmndsRegister(int climatePort, int lightPort, int utilityPort)	throws InterruptedException {
-
-			LightData myLightData = new LightData();
-
+	 
+	 public void jmdnsRegister(int climatePort, int lightPort, int utilityPort)	throws InterruptedException {
+	 
 			try {
 				// Create a JmDNS instance
 				jmdns = JmDNS.create(InetAddress.getLocalHost());
 				// Registering all services
 				System.out.println("Register Service");
-				ServiceInfo serviceUtil = ServiceInfo.create("_http._tcp.local.", "utility", utilityPort, "path=index.html");
-				ServiceInfo serviceClime = ServiceInfo.create("_http._tcp.local.", "climate", climatePort, "path=index.html");
-				ServiceInfo serviceLight = ServiceInfo.create("_http._tcp.local.", myLightData.gettype(), lightPort,"path=index.html");
+				ServiceInfo serviceUtil = ServiceInfo.create("_http._tcp.local.", "utility", 50098, "path=index.html");
+				ServiceInfo serviceClime = ServiceInfo.create("_http._tcp.local.", "climate", 50099, "path=index.html");
+				ServiceInfo serviceLight = ServiceInfo.create("_http._tcp.local.", "light", 50097,"path=index.html");
 
 				jmdns.registerService(serviceUtil);
 				jmdns.registerService(serviceClime);
 				jmdns.registerService(serviceLight);
-
+				System.out.println("Registration Service Start ! ");
+				
 				 //Wait
 				 Thread.sleep(10000);
 
+				 //jmdns.unregisterAllServices();
 				
 			} catch (IOException e) {
 				System.out.println(e.getMessage());
 			}
+	 }
+			
+			public void unRegister() {
+				System.out.println("Un-registering");
+
+				jmdns.unregisterAllServices();
+
+			}
+
 		}
+	
 
-		public void unRegister() {
-			System.out.println("Unregistering services");
 
-			jmdns.unregisterAllServices();
-
-		}
-}
