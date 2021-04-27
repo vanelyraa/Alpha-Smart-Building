89a50fb9ac259aeef3c3d57986f5062c6b5db7a2
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-27 11:48:24 +0100

    Update ServiceDiscovery.java

--- ALPHASMARTB/src/main/java/jmDNS/ServiceDiscovery.java
+++ ALPHASMARTB/src/main/java/jmDNS/ServiceDiscovery.java
@@ -15,7 +15,7 @@ public class ServiceDiscovery {
 		
 		
 		public void serviceAdded(ServiceEvent event) {
-			System.out.println("Service addedPORT?: " + event.getInfo().getPort());
+			System.out.println("Service added: " + event.getInfo());
 		}
 
 		
@@ -26,8 +26,13 @@ public class ServiceDiscovery {
 		
 		public void serviceResolved(ServiceEvent event) {
 					System.out.println("Service resolved: " + event.getInfo());
-			
 					System.out.println("Get Name: " + event.getName() + " PORT: " + event.getInfo().getPort());
+					/*ServiceInfo info = event.getInfo();
+		            int port = info.getPort();
+		            String path = info.getNiceTextString().split("=")[1];
+
+		            String url = "http://localhost:" + port + "/" + path;
+		            System.out.println(" --- sending request to " + url);
 					
 		}
 	}
