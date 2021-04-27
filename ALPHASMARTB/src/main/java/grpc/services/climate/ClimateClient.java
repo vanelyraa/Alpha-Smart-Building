ccc3c91abf7c04a57473284f4762ca74b01f5a8f
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-27 16:54:51 +0100

    Update ClimateClient.java

--- ALPHASMARTB/src/main/java/grpc/services/climate/ClimateClient.java
+++ ALPHASMARTB/src/main/java/grpc/services/climate/ClimateClient.java
@@ -1,15 +1,12 @@
 package grpc.services.climate;
 
-
-/*import java.io.IOException;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-
 import javax.jmdns.JmDNS;
 import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceInfo;
 import javax.jmdns.ServiceListener;
-
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
@@ -18,6 +15,7 @@ public class ClimateClient {
 	
 	private static ClimateServiceGrpc.ClimateServiceBlockingStub cblockingStub;
 	private static ClimateServiceGrpc.ClimateServiceStub casyncStub;
+	public static int randomCo = (int)(Math.random() * 100 + 1);
 	
 	public static class Listener implements ServiceListener {
         @Override
@@ -36,8 +34,6 @@ public class ClimateClient {
             ServiceInfo info = serviceEvent.getInfo();
             final int Port = serviceEvent.getInfo().getPort();
             String address = info.getHostAddresses()[0];
-            //String address = "localhost";
-            
             
         }
     }
@@ -50,36 +46,30 @@ public class ClimateClient {
 		cblockingStub = ClimateServiceGrpc.newBlockingStub(climatechannel);
 		casyncStub = ClimateServiceGrpc.newStub(climatechannel);
 		
-		try {
-			// Create a JmDNS instance
-			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
-
-			// Add a service listener
+		try {			
+			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());			
 			jmdns.addServiceListener("_http._tcp.local.", new Listener());
-
 		} catch (UnknownHostException e) {
 			System.out.println(e.getMessage());
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 		}
 		
-		HvacOnOff();
+		hvacOnOff();
 		HvacTemperature();
 		checkCO();
 		
 	}
 	
-	public static void HvacOnOff(){
+	public static void hvacOnOff(){
 		
 		SwitchRequest request = SwitchRequest.newBuilder().setPower(false).build();
 
 		SwitchResponse response = cblockingStub.hvacOnOff(request);
-
 		if (response.getPower()) {
-			System.out.println("HVAC is on!");
-		}
-		else {
-			System.out.println("HVAC is off!");
+			System.out.println("HVAC off!");
+		} else {
+			System.out.println("HVAC on!");
 		}
 	}
 
@@ -87,17 +77,18 @@ public class ClimateClient {
 		
 		HvacRequest request = HvacRequest.newBuilder().setTemp(20).build();
 		int newTemp = request.getTemp();
-		System.out.println("Set room temperature to " + request.getTemp() + " °C");
+		
+		System.out.println("Requesting temperature change to " + request.getTemp() + " °C");
 
 		StreamObserver<HvacResponse> responseObserver = new StreamObserver<HvacResponse>() {
 
 			@Override
-			public void onNext(HvacResponse tempNew) {
+			public void onNext(HvacResponse value) {
 				if(newTemp > 35 || newTemp < 15 ) {//start if
-					System.out.println("Select temperature between 15°C and 35°C: ");
+					System.out.println("Select temperature between 15°C and 35°C ");
 				} else {
-				System.out.println("Temperature changed: " + tempNew.getTemp() + " °C");
-			}
+				System.out.println("Changing temperature to: " + value.getTemp() + " °C");
+				}
 			}
 	
 			@Override
@@ -109,12 +100,9 @@ public class ClimateClient {
 			public void onCompleted() {
 				System.out.println("Room reached the selected temperature: "+request.getTemp()+"°C");
 			}
-
 		};
 
 		casyncStub.hvacTemperature(request, responseObserver);
-
-
 		try {
 			Thread.sleep(2000);
 		} catch (InterruptedException e) {
@@ -124,16 +112,16 @@ public class ClimateClient {
 	
 	public static void checkCO(){
 		
-		CoLevelRequest request = CoLevelRequest.newBuilder().setLevel(25).build();
+		CoLevelRequest request = CoLevelRequest.newBuilder().setLevel(randomCo).build();
 
 		ExtractionResponse response = cblockingStub.checkCO(request);
-
 		if (response.getLevel() > 40) {
-			System.out.println("Co level is: " + response.getLevel() + "now");
+			System.out.println("Co level is: " + response.getLevel() + " now");
 			System.out.println("High level of CO, extractor is on!");
 		}
 		else {
-			System.out.println("Extractor off!");
+			System.out.println("Co level is: " + response.getLevel() + " now");
+			System.out.println("CO level OK!");
 		}
 	}
-}*/
+}
