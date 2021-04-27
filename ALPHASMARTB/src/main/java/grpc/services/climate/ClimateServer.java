6d0db043dd008038e49379c348e133738b08e05c
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-27 16:54:54 +0100

    Update ClimateServer.java

--- ALPHASMARTB/src/main/java/grpc/services/climate/ClimateServer.java
+++ ALPHASMARTB/src/main/java/grpc/services/climate/ClimateServer.java
@@ -14,56 +14,50 @@ public class ClimateServer extends ClimateServiceImplBase{
 	
 public static void main(String[] args) throws IOException, InterruptedException {
 		
-		System.out.println("Starting gRPC Climate Server");
+	System.out.println("Starting gRPC Climate Server");		
 		
-		
-		try {
-			int PORT = 50099;
-			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
-			ServiceInfo serviceClimate = ServiceInfo.create("_http._tcp.local.", "climate", 50099, "path=index.html");
-	        jmdns.registerService(serviceClimate);
-	        ClimateServer climateServer = new ClimateServer();
-	        Server server = ServerBuilder.forPort(PORT)
-                    .addService(climateServer)
-                    .build()
-                    .start();
-            System.out.println("Climate server started, listening on " + PORT);
-            server.awaitTermination();
-            
+	try {
+		int PORT = 50099;
+		JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+		ServiceInfo serviceClimate = ServiceInfo.create("_http._tcp.local.", "climate", 50099, "path=index.html");
+	    jmdns.registerService(serviceClimate);
+	    ClimateServer climateServer = new ClimateServer();
+	    Server server = ServerBuilder.forPort(PORT)
+	    	.addService(climateServer)
+	    	.build()
+            .start();
+        
+	    System.out.println("Climate server started, listening on " + PORT);
+        server.awaitTermination();            
 		} catch (UnknownHostException e) {
 			System.out.println(e.getMessage());
             e.printStackTrace();
 		} catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
-        }
-		
+        }		
 	}
-
 
 	@Override
 	public void hvacOnOff(SwitchRequest request, StreamObserver<SwitchResponse> responseObserver) {
 		
-		System.out.println("Receiving request to turn on HVAC!");
+		System.out.println("Receiving request to turn on/off HVAC!");
 
         boolean OnOffH = request.getPower();
-        if (OnOffH) {
-        	System.out.println("Setting power to on!");
-        }
-        else {
-        	System.out.println("Setting power to off!");
+        if (request.getPower()) {
+        	System.out.println("Setting HVAC off!");
+        } else {
+        	System.out.println("Setting HVAC on!");
         }
         
         SwitchResponse response = SwitchResponse.newBuilder().setPower(OnOffH).build();
-
         responseObserver.onNext(response);
         responseObserver.onCompleted();
-
 	}
 
 	@Override
 	public void hvacTemperature(HvacRequest request, StreamObserver<HvacResponse> responseObserver) {
-int newTemp = request.getTemp();
+		int newTemp = request.getTemp();
 		
 		System.out.println("Receiving request to change temperature to: "+ newTemp +"°C");
 		try {
@@ -71,30 +65,31 @@ int newTemp = request.getTemp();
 			HvacResponse response1 = HvacResponse.newBuilder().setTemp(newTemp-1).build();
 			HvacResponse response2 = HvacResponse.newBuilder().setTemp(newTemp).build();
 			
-			// delaying the response to simulate the system heating to the desired temperature
-			try {
-	            Thread.sleep(2000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-			 responseObserver.onNext(response);
-			 try {
-	            Thread.sleep(2000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-			 responseObserver.onNext(response1);
-			 try {
-	            Thread.sleep(2000);
-	        } catch (InterruptedException e) {
-	            e.printStackTrace();
-	        }
-			 responseObserver.onNext(response2);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		responseObserver.onNext(response);
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		responseObserver.onNext(response1);
+			 
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		responseObserver.onNext(response2);
+		
 		} catch (Error e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-
 		responseObserver.onCompleted();
 	}
 	
@@ -109,17 +104,10 @@ int newTemp = request.getTemp();
         	responseObserver.onNext(ExtractionResponse.newBuilder().setLevel(CoLv).build());
         	System.out.println("CO level is: " + CoLv);
         		System.out.println("High level of CO, extractor is on!");
-		}      
-        else {
+		} else {
         	responseObserver.onNext(ExtractionResponse.newBuilder().setLevel(CoLv).build());
         	System.out.println("CO level OK!");
-        }
-        
+        }        
         responseObserver.onCompleted();
-
 	}
-
-
-	
-
 }
