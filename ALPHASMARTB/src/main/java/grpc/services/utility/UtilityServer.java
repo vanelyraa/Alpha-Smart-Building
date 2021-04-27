7a37e5031f1913ec32a19db4127011de7880d711
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-27 11:48:20 +0100

    Update UtilityServer.java

--- ALPHASMARTB/src/main/java/grpc/services/utility/UtilityServer.java
+++ ALPHASMARTB/src/main/java/grpc/services/utility/UtilityServer.java
@@ -3,14 +3,8 @@ package grpc.services.utility;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.logging.Logger;
-
 import javax.jmdns.JmDNS;
-import javax.jmdns.ServiceEvent;
 import javax.jmdns.ServiceInfo;
-import javax.jmdns.ServiceListener;
-
-import grpc.services.light.LightServer;
 import grpc.services.utility.UtilityServiceGrpc.UtilityServiceImplBase;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
@@ -19,46 +13,45 @@ import io.grpc.stub.StreamObserver;
 
 public class UtilityServer extends UtilityServiceImplBase{
 	
-	        
 	public static void main(String[] args) throws IOException, InterruptedException {
-System.out.println("Starting gRPC Utilities Server");
-		
-		// Create & Register utilities service with jmDNS
-		try {
-			int PORT = 50052;
-			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
-	        ServiceInfo serviceInfo = ServiceInfo.create("_utilitiy._tcp.local.", "utilitiy", PORT, "Utilitiy server");
-	        jmdns.registerService(serviceInfo);
-	        UtilityServer utilityServer = new UtilityServer();
-	        Server server = ServerBuilder.forPort(PORT)
-                    .addService(utilityServer)
-                    .build()
-                    .start();
-            System.out.println("Utility server started, listening on " + PORT);
-            server.awaitTermination();
-            
-		} catch (UnknownHostException e) {
-			System.out.println(e.getMessage());
-            e.printStackTrace();
-		} catch (IOException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-        }
-		
-	}
 	
+	System.out.println("Starting gRPC Utilities Server");
+	
+	
+	try {
+		int PORT = 50098;
+		JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());
+		ServiceInfo serviceUtility = ServiceInfo.create("_http._tcp.local.", "utility", 50098, "path=index.html");
+        jmdns.registerService(serviceUtility);
+        UtilityServer utilityServer = new UtilityServer();
+        Server server = ServerBuilder.forPort(PORT)
+                .addService(utilityServer)
+                .build()
+                .start();
+        System.out.println("Utility server started, listening on " + PORT);
+        server.awaitTermination();
+        
+	} catch (UnknownHostException e) {
+		System.out.println(e.getMessage());
+        e.printStackTrace();
+	} catch (IOException e) {
+        System.out.println(e.getMessage());
+        e.printStackTrace();
+    }
+	
+}
 	
 	@Override
 	public void switchDevices(DevicesRequest request, StreamObserver<DevicesResponse> responseObserver) {
 		
-		System.out.println("Receiving request to turn off devices!");
+		System.out.println("Receiving request to turn On devices!");
 
         boolean OnOffD = request.getDevices();
         if (OnOffD) {
-        	System.out.println("Setting power to off!");
+        	System.out.println("Setting power to on!");
         }
         else {
-        	System.out.println("Setting power to on!");
+        	System.out.println("Setting power to off!");
         }
         
         DevicesResponse response = DevicesResponse.newBuilder().setDevices(OnOffD).build();
@@ -71,7 +64,7 @@ System.out.println("Starting gRPC Utilities Server");
 	@Override
 	public void switchCameraOn(CameraRequest request, StreamObserver<CameraResponse> responseObserver) {
 		
-		System.out.println("Receiving request to turn cameras on!");
+		System.out.println("Receiving request to turn On cameras!");
 
         boolean OnOffC = request.getCamera();
         if (OnOffC) {
@@ -100,7 +93,7 @@ System.out.println("Starting gRPC Utilities Server");
 			}
 
 	        public void onError(Throwable t) {
-	        	System.out.println("Error on printing service");
+	        	System.out.println("Printing server failed");
 	        }
 	        
 	        public void onCompleted() {
