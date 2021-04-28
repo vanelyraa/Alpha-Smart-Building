d80772ded45bfbfb5dc59e7a01b3b977ea2f748f
vanelyra <76132238+vanelyra@users.noreply.github.com>
2021-04-28 19:12:21 +0100

    Update ClimateClient.java

--- ALPHASMARTB/src/main/java/grpc/services/climate/ClimateClient.java
+++ ALPHASMARTB/src/main/java/grpc/services/climate/ClimateClient.java
@@ -11,23 +11,28 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
 
+//CLIENT IMPLEMENTATION
 public class ClimateClient {
 	
 	private static ClimateServiceGrpc.ClimateServiceBlockingStub cblockingStub;
 	private static ClimateServiceGrpc.ClimateServiceStub casyncStub;
 	public static int randomCo = (int)(Math.random() * 100 + 1);
 	
+	//Add listener for discovery
 	public static class Listener implements ServiceListener {
+		//service resolution
         @Override
         public void serviceAdded(ServiceEvent serviceEvent) {
             System.out.println("Service added: " + serviceEvent.getInfo());
         }
 
+        //removed service
         @Override
         public void serviceRemoved(ServiceEvent serviceEvent) {
             System.out.println("Service removed: " + serviceEvent.getInfo());
         }
 
+        //service resolved.
         @Override
         public void serviceResolved(ServiceEvent serviceEvent) {
             System.out.println("Service resolved: " + serviceEvent.getInfo());
@@ -41,13 +46,18 @@ public class ClimateClient {
 	
 	public static void main(String[] args) throws IOException, InterruptedException{
 		
+		//GRPC channels
 		ManagedChannel climatechannel = ManagedChannelBuilder.forAddress("localhost", 50099).usePlaintext().build();
 
+		//stubs -- generate from proto
 		cblockingStub = ClimateServiceGrpc.newBlockingStub(climatechannel);
 		casyncStub = ClimateServiceGrpc.newStub(climatechannel);
 		
-		try {			
-			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());			
+		try {	
+			// Create a JmDNS instance
+			JmDNS jmdns = JmDNS.create(InetAddress.getLocalHost());	
+			
+			// Add a service listener
 			jmdns.addServiceListener("_http._tcp.local.", new Listener());
 		} catch (UnknownHostException e) {
 			System.out.println(e.getMessage());
@@ -61,11 +71,17 @@ public class ClimateClient {
 		
 	}
 	
+	/**
+	 * GRPC services
+	 */
+
+	//Switch HVAC
 	public static void hvacOnOff(){
 		
 		SwitchRequest request = SwitchRequest.newBuilder().setPower(false).build();
 
 		SwitchResponse response = cblockingStub.hvacOnOff(request);
+		//if true, HVAC off otherwise, on
 		if (response.getPower()) {
 			System.out.println("HVAC off!");
 		} else {
@@ -73,15 +89,18 @@ public class ClimateClient {
 		}
 	}
 
+	//Control temperature
 	public static void HvacTemperature(){
 		
 		HvacRequest request = HvacRequest.newBuilder().setTemp(20).build();
 		int newTemp = request.getTemp();
 		
+		//notification of method invocation
 		System.out.println("Requesting temperature change to " + request.getTemp() + " °C");
 
 		StreamObserver<HvacResponse> responseObserver = new StreamObserver<HvacResponse>() {
-
+			
+			//minimum and max temperature check
 			@Override
 			public void onNext(HvacResponse value) {
 				if(newTemp > 35 || newTemp < 15 ) {//start if
@@ -110,10 +129,12 @@ public class ClimateClient {
 		}
 	}
 	
+	//Check CO level in the room
 	public static void checkCO(){
 		
 		CoLevelRequest request = CoLevelRequest.newBuilder().setLevel(randomCo).build();
-
+		
+		//if CO level is over 40, extractors will be turned on
 		ExtractionResponse response = cblockingStub.checkCO(request);
 		if (response.getLevel() > 40) {
 			System.out.println("Co level is: " + response.getLevel() + " now");
